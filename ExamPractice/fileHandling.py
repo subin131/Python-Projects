32ce0deedda35cef98b1709ae976e8cd504c5955
satyal <satyalsubin33@gmail.com>
2022-07-24 22:48:25 +0545

    wrting the conetnt into the file

---
+++ ExamPractice/fileHandling.py
@@ -0,0 +1,12 @@
+# file=open("fileHandling.txt","a")
+# print(file.write("This is test..."))
+
+
+# f=open("fileHandling.txt","r")
+# print(f.read())
+
+
+# reading the txt files from the different directory
+
+f=open(r"/home/test.txt","r")
+print(f.read())

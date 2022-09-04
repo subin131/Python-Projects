f86bed4a75435022adb7612bd9cea40b42bb387f
satyal <satyalsubin33@gmail.com>
2022-09-04 21:15:15 +0545

    some changes in code

--- MultipleEmployeeSalary.py
+++ MultipleEmployeeSalary.py
@@ -12,10 +12,29 @@ def inputDisplay():
     for i in range(number):
         name=input("Enter the name: ")
         salary=int(input("Enter the salary: "))
+        choice=input("Enter Y for married and N for unmarried: ")
         
-        employees[i]={f"name: {name},salary:{salary}"}
+        employees[i]={f"name: {name},salary:{salary},choice:{choice}"}
         
     return employees
+
+def conditionalChoice():
+    data=inputDisplay()
+    for i in range(data):
+        choice=data[i+2]
+        if choice=="Y":
+            tax=marriedTax()
+        else:
+            tax=unmarriedTtax()
+            
+    return tax
+
+def marriedTax():
+    pass
+
+def unmarriedTax():
+    pass
+
 
 
 print(inputDisplay())

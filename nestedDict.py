26906ae4741d4024f87e876e9af936e13e3328db
satyal <satyalsubin33@gmail.com>
2022-09-05 21:16:26 +0545

    adding the nested dictionary

---
+++ nestedDict.py
@@ -0,0 +1,6 @@
+people = {1: {'name': 'John', 'age': '27', 'sex': 'Male'},
+          2: {'name': 'Marie', 'age': '22', 'sex': 'Female'}}
+
+print(people[1]['name'])
+print(people[1]['age'])
+print(people[1]['sex'])

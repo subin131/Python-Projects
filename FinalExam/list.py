d3cf92db343166b1823ec3386e0cd47a5e591b03
satyal <satyalsubin33@gmail.com>
2022-09-07 23:47:41 +0545

    list with their common features

---
+++ FinalExam/list.py
@@ -0,0 +1,13 @@
+list=[1,2,3,4,4]
+# adding the element in last position
+list.append(2)
+# insert will add the elements into the specific position where the first parameter is for index and second for valur to be added
+list.insert(0,7)
+
+#delete the specified index value
+list.pop(2)
+
+list.pop()
+for i in list:
+    print(i)
+    

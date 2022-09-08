4bedb7fc6c523e9ee32b87063fd5d17c1e874eb8
satyal <satyalsubin33@gmail.com>
2022-09-08 07:03:30 +0545

    adding the list item using insert function

---
+++ FinalExam/list/insert.py
@@ -0,0 +1,6 @@
+#insert will add the item at the specified index
+#for example
+numbers=[1,2,3,4,5]
+#here the insert method have two parameter where first is index and second is the value to be added into that index
+numbers.insert(3,11)
+print(numbers)

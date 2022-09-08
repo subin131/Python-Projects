9511ac479bd14537fc495717e72b26ffb5591686
satyal <satyalsubin33@gmail.com>
2022-09-08 07:05:14 +0545

    removing the value using remove function and pass the value as parameter

---
+++ FinalExam/list/remove.py
@@ -0,0 +1,5 @@
+#remove will remove specified value from the list
+numbers=[1,2,3,4,5,0]
+#here the remove method will remove the value whatwver the value pass into that function
+numbers.remove(5)
+print(numbers)

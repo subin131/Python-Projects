26a69b7aa72f8f1c7b6bb61b0a0fae37fde055d5
satyal <satyalsubin33@gmail.com>
2022-09-08 07:04:17 +0545

    removing the list from last poistion

---
+++ FinalExam/list/pop.py
@@ -0,0 +1,5 @@
+#pop is anoher removing method where it will remove an element at given index
+numbers=[1,2,3,4,5]
+#it will remove an item from specified index of the list
+numbers.pop(2)
+print(numbers)

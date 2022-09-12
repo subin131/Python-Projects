76647ce38afbba8374576329fc6dc9181d2d9000
satyal <satyalsubin33@gmail.com>
2022-09-12 06:15:55 +0545

    adding the new item in existing list uisng append

---
+++ FinalExam/list/append.py
@@ -0,0 +1,11 @@
+#adding all the builtint methods of the list
+#first of all make the list of numbers
+numbers=[1,2,4,5,7,8]
+
+#now append methods is to add an elements to the end of the list
+numbers.append(10)
+#testing
+print(numbers)
+
+
+
